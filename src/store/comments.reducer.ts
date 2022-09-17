d5568389b137efaf48bda29701ab41c673f30b1e
정재훈 <jhc060109@naver.com>
2022-09-17 11:09:41 +0900

    feat: comment reducer 개발 (진행 중)

--- src/store/comments.reducer.ts
+++ src/store/comments.reducer.ts
@@ -28,32 +28,37 @@ const initialState: CommentState = {
 };
 
 // GET
-const getComments = createAsyncThunk("comment/getComments", async () => {
-  const response = await axiosClient.get("/comments");
-  console.log("get comments: ", response);
-  return response;
-});
+export const getComments = createAsyncThunk(
+  "comment/getComments",
+  async (pageNum: number = 1) => {
+    const response = await axiosClient.get<Comment[]>(
+      `/comments?_page=${pageNum}&_limit=4&_order=desc&_sort=id`
+    );
+    console.log("get comments: ", response);
+    return response.data;
+  }
+);
 
 // PUT
-const putComments = createAsyncThunk(
-  "comment/putCommnets",
+export const putComments = createAsyncThunk(
+  "comment/putComments",
   async (comment: Comment) => {
     const response = await axiosClient.put(`/comments/${comment.id}`, comment);
-    return response;
+    return response.data;
   }
 );
 
 // POST
-const postComments = createAsyncThunk(
+export const postComments = createAsyncThunk(
   "comment/postComments",
   async (comment: Comment) => {
     const response = await axiosClient.post("/comments", comment);
-    return response;
+    return response.data;
   }
 );
 
 // DELETE
-const deleteComments = createAsyncThunk(
+export const deleteComments = createAsyncThunk(
   "comment/deleteComments",
   async (comment: Comment) => {
     const response = await axiosClient.delete(`/comments/${comment.id}`);
@@ -78,10 +83,54 @@ export const commentsSlice = createSlice({
   extraReducers: (builder) => {
     // GET
     builder.addCase(getComments.pending, (state) => {
-      // state를 어떻게 다루냐는 여기서...
+      state.isLoading = true;
     });
-    builder.addCase(getComments.fulfilled, (state, action) => {});
-    builder.addCase(getComments.rejected, (state, action) => {});
+    builder.addCase(getComments.fulfilled, (state, action) => {
+      state.comments = [...action.payload];
+      state.isLoading = false;
+    });
+    builder.addCase(getComments.rejected, (state, action) => {
+      alert("데이터를 받는데 문제가 발생했습니다...");
+      state.isLoading = false;
+    });
+
+    // PUT
+    builder.addCase(putComments.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(putComments.fulfilled, (state, action) => {
+      // state.comments = [...action.payload];
+      console.log("수정된 데이터", [...action.payload]);
+      state.isLoading = false;
+    });
+    builder.addCase(putComments.rejected, (state, action) => {
+      alert("수정에 실패하였습니다.");
+      state.isLoading = false;
+    });
+
+    // POST
+    builder.addCase(postComments.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(postComments.fulfilled, (state) => {
+      state.isLoading = false;
+    });
+    builder.addCase(postComments.rejected, (state) => {
+      alert("댓글 작성에 실패하였습니다.");
+      state.isLoading = false;
+    });
+
+    // DELETE
+    builder.addCase(deleteComments.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(deleteComments.fulfilled, (state) => {
+      state.isLoading = false;
+    });
+    builder.addCase(deleteComments.rejected, (state) => {
+      alert("댓글 삭제에 실패하였습니다.");
+      state.isLoading = false;
+    });
   },
 });
 
