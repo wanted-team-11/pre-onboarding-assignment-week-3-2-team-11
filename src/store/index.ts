51edbab831a06a9764aa39fd781d3549c67216b0
정재훈 <jhc060109@naver.com>
2022-09-17 16:25:10 +0900

    chore: logger 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,16 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import commentsReducer from "./comments.reducer";
 import formReducer from "./form.reducer";
+import logger from "redux-logger";
 
 const reducer = combineReducers({
   form: formReducer,
-  commets: commentsReducer,
+  comments: commentsReducer,
 });
 
 export const store = configureStore({
   reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
